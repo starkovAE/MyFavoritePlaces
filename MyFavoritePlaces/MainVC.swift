5f92474831e9f2631bda0b38d6d72ec9ea50a5a5
starkovAE <swix456@gmail.com>
2022-03-24 13:23:36 +0500

    Three commit. Add icon, Launch screen, Row hight

--- MyFavoritePlaces/MainVC.swift
+++ MyFavoritePlaces/MainVC.swift
@@ -37,10 +37,16 @@ class MainVC: UITableViewController {
 
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
-
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         return cell
     }
-
+//MARK: - TableView Delegate
+    //метод отвечает за высоту строк
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
+    
     /*
     // MARK: - Navigation
 
