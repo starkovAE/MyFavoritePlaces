069a6ac47d94c85936840e80c02da61a9b119ec8
starkovAE <swix456@gmail.com>
2022-03-24 15:13:57 +0500

    Four Commit. custom table view cell

---
+++ MyFavoritePlaces/CustomTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTableViewCell.swift
+//  MyFavoritePlaces
+//
+//  Created by Александр Старков on 24.03.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
